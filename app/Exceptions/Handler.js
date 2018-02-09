950d4e13907633901a8c21819a29bbfc977af3e2
Harminder <virk.officials@gmail.com>
2018-02-09 16:51:09 +0530

    chore(package): upgrade to 4.1

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,12 +1,14 @@
 'use strict'
 
+const BaseExceptionHandler = use('BaseExceptionHandler')
+
 /**
  * This class handles all exceptions thrown during
  * the HTTP request lifecycle.
  *
  * @class ExceptionHandler
  */
-class ExceptionHandler {
+class ExceptionHandler extends BaseExceptionHandler {
   /**
    * Handle exception thrown during the HTTP lifecycle
    *
@@ -20,26 +22,12 @@ class ExceptionHandler {
    */
   async handle (error, ctx) {
     if (process.env.NODE_ENV === 'development') {
-      await make('Adonis/Exceptions/Handler').handle(error, ctx)
-      return
+      return BaseExceptionHandler.handle(error, ctx)
     }
 
     const { response, view } = ctx
     const status = error.status || 500
     response.status(status).send(view.render('error', { status }))
-  }
-
-  /**
-   * Report exception for logging or debugging.
-   *
-   * @method report
-   *
-   * @param  {Object} error
-   * @param  {Object} options.request
-   *
-   * @return {void}
-   */
-  async report (error, { request }) {
   }
 }
 
