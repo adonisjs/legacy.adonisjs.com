115e9bb031f78b470737620439d63cd4a23e9765
Harminder Virk <virk.officials@gmail.com>
2018-09-27 06:15:25 +0200

    chore(package): update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['@babel/preset-env']
           }
         }
       },
