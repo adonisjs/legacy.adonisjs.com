53f0b61ea4827da85aff8a8385b1ea2bf9e1f2da
Romain Lanz <romain.lanz@slynova.ch>
2020-03-02 23:02:01 +0100

    style: add section selection to mobile

--- frontend/src/components/Guides/Sidebar.vue
+++ frontend/src/components/Guides/Sidebar.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="sidebar-wrapper">
-    <div>
-      <select name="docs-menu" class="menu-select-box" @change="openUrl($event)">
-        <optgroup v-for="(category, index) in categories" :key="index" :label="category.name">
-          <option v-for="doc in category.docs" :key="doc.permalink" :value="`/${doc.permalink}`">
-            {{ doc.title }}
+    <div class="menus-wrapper">
+      <div class="menu-select-box--wrapper">
+        <select name="group-menu" class="menu-select-box" @change="openUrl($event)">
+          <option v-for="group in groups" :key="group.name" :value="`/${group.categories[0].docs[0].permalink}`">
+            {{ group.name }}
           </option>
-        </optgroup>
-      </select>
+        </select>
+
+        <svg width="8" height="6" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M.94.727l-.94.94 4 4 4-4-.94-.94L4 3.78.94.727z" fill="currentColor"/></svg>
+      </div>
+
+      <div class="menu-select-box--wrapper">
+        <select name="docs-menu" class="menu-select-box" @change="openUrl($event)">
+          <optgroup v-for="(category, index) in categories" :key="index" :label="category.name">
+            <option v-for="doc in category.docs" :key="doc.permalink" :value="`/${doc.permalink}`">
+              {{ doc.title }}
+            </option>
+          </optgroup>
+        </select>
+
+        <svg width="8" height="6" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M.94.727l-.94.94 4 4 4-4-.94-.94L4 3.78.94.727z" fill="currentColor"/></svg>
+      </div>
     </div>
 
     <aside class="sidebar">
@@ -132,10 +146,32 @@
     color: var(--brand);
   }
 
-  .sidebar-wrapper .menu-select-box {
-    width: 100%;
-    padding: 10px;
+  .menus-wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 4rem;
+  }
+
+  .sidebar-wrapper .menu-select-box--wrapper {
+    border: 1px solid var(--grey-100);
+    border-radius: 3px;
     margin-top: 40px;
+    position: relative;
+  }
+
+  .sidebar-wrapper .menu-select-box {
+    appearance: none;
+    height: 4rem;
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+
+  .menu-select-box--wrapper svg {
+    position: absolute;
+    right: 18px;
+    top: 50%;
+    transition: 100ms transform ease;
+    transform: translateY(-50%);
   }
 
   @media (min-width: 1024px) {
@@ -163,12 +199,14 @@
       padding: 10px 30px 10px 20px;
       position: relative;
     }
+
     .dropdown-selected-item span {
       text-overflow: ellipsis;
       white-space: nowrap;
       display: block;
       overflow: hidden;
     }
+
     .dropdown-selected-item svg {
       position: absolute;
       right: 18px;
@@ -176,6 +214,7 @@
       transition: 100ms transform ease;
       transform: translateY(-50%);
     }
+
     .dropdown-options {
       box-shadow: 0 20px 40px 0 rgba(0,0,0,0.02), 0 14px 6px 0 rgba(0,0,0,0.03);
       border: 1px solid var(--grey-100);
