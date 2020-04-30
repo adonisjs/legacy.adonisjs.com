c59c1525cb42a8dbf5e65d449a24ddc5a4312634
Romain Lanz <romain.lanz+github@pm.me>
2020-04-30 18:27:17 +0200

    feat(Time): add DateTime component

---
+++ frontend/src/components/Time/DateTime.vue
@@ -0,0 +1,29 @@
+<template>
+	<span>{{ formatedTime }}</span>
+</template>
+
+<script>
+	import Vue from 'vue'
+  import { format, parseISO } from 'date-fns'
+
+	export default {
+		props: {
+			time: {
+				type: String,
+				required: true,
+      },
+
+			format: {
+				type: String,
+				required: false,
+				default: 'dd.MM.yyyy',
+			},
+		},
+		computed: {
+			formatedTime() {
+				const parsedDate = parseISO(this.time)
+				return format(parsedDate, this.format)
+			},
+		},
+	}
+</script>
