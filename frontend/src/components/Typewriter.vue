cb7d18661d56f503b624b8fe09ecff27d612fcba
Romain Lanz <romain.lanz@slynova.ch>
2020-05-16 15:07:02 +0200

    feat(Typewriter): add typewriter system

---
+++ frontend/src/components/Typewriter.vue
@@ -0,0 +1,31 @@
+<template>
+  <span ref="word" />
+</template>
+
+<script>
+import TypeIt from 'typeit'
+
+export default {
+  props: {
+    words: {
+      type: Array,
+      required: true
+    },
+
+    delay: {
+      type: Number,
+      default: 4000
+    }
+  },
+
+  mounted () {
+    new TypeIt(this.$refs.word, {
+      strings: this.words,
+      nextStringDelay: [this.delay, 750],
+      loopDelay: this.delay,
+      breakLines: false,
+      loop: true
+    }).go()
+  }
+}
+</script>
