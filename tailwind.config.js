7bee38a83f111f4f1e55be358fa3a2297fdc3e2c
Romain Lanz <romain.lanz@pm.me>
2020-08-30 00:14:23 +0200

    chore: setup custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        adonis: {
+          black: '#17161a',
+          'grey-dark': '#666976',
+          inactive: '#b1bacc',
+          background: '#f0f3f9',
+          background2: '#f7f8fa',
+          outline: '#d7deeb',
+          brand: '#5a45ff',
+          green: '#70d787',
+          red: '#df4a4a',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
