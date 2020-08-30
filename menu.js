c48bb5cfe5a50b0e892d61b03262f1d8b9af04a0
Romain Lanz <romain.lanz@pm.me>
2020-08-30 14:32:52 +0200

    style: wip

--- menu.js
+++ menu.js
@@ -7,6 +7,11 @@ module.exports = {
 				name: 'Handling HTTP Requests',
 				children: [
 					{ name: 'Introduction', link: 'http/introduction' },
+					{ name: 'Routing', link: 'http/routing' },
+					{ name: 'Controllers', link: 'http/controllers' },
+					{ name: 'Views and Templates', link: 'http/views-and-templates' },
+					{ name: 'File Uploads', link: 'http/file-uploads' },
+					{ name: 'Sessions', link: 'http/sessions' },
 				],
 			},
 		],
