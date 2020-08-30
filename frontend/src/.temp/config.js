bf26370e59ebd720c5d6f66b63376d6935a0e05c
thetutlage <virk.officials@gmail.com>
2020-08-30 19:17:23 +0530

    remove unwanted files

--- frontend/src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - AdonisJS",
-  "siteUrl": "https://preview.adonisjs.com",
-  "version": "0.7.19",
-  "catchLinks": true
-}
