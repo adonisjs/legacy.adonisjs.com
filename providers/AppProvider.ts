fc8019c5e3c79be9e75314d84cbbf2cb241e1134
Harminder Virk <virk.officials@gmail.com>
2020-12-07 22:39:11 +0530

    feat: initiate adonis app

---
+++ providers/AppProvider.ts
@@ -0,0 +1,23 @@
+import { ApplicationContract } from '@ioc:Adonis/Core/Application'
+
+export default class AppProvider {
+  public static needsApplication = true
+
+  constructor(protected app: ApplicationContract) {}
+
+  public register() {
+    // Register your own bindings
+  }
+
+  public async boot() {
+    // IoC container is ready
+  }
+
+  public async ready() {
+    // App is ready
+  }
+
+  public async shutdown() {
+    // Cleanup, since app is going down
+  }
+}
