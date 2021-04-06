4c8958f3208441550e9ba9f36722523c25279928
Romain Lanz <romain.lanz+github@pm.me>
2021-04-06 21:56:16 +0200

    feat: add back Discord invite link

--- frontend/config.js
+++ frontend/config.js
@@ -32,6 +32,11 @@ exports.header = [
     isExternal: false,
     children: [
       {
+        title: 'Discord',
+        permalink: 'https://discord.gg/vDcEjq6',
+        isExternal: true,
+      },
+      {
         title: 'Github',
         permalink: 'https://github.com/adonisjs',
         isExternal: true,
@@ -45,8 +50,8 @@ exports.header = [
         title: 'Twitter',
         permalink: 'https://twitter.com/adonisframework',
         isExternal: true,
-      }
-    ]
+      },
+    ],
   },
 ]
 
@@ -76,7 +81,7 @@ exports.footer = [
         permalink: '/releases/core/preview-rc-5',
         isExternal: false,
       },
-    ]
+    ],
   },
   {
     title: 'Community',
@@ -97,8 +102,8 @@ exports.footer = [
         title: 'Twitter',
         permalink: 'https://twitter.com/adonisframework',
         isExternal: true,
-      }
-    ]
+      },
+    ],
   },
   {
     title: 'About',
@@ -120,7 +125,7 @@ exports.footer = [
         permalink: '/release-process',
         isExternal: false,
       },
-    ]
+    ],
   },
 ]
 
@@ -151,7 +156,7 @@ Route<span class="token punctuation">.</span><span class="token keyword">get</sp
  * Using Controller
  */</span>
 Route<span class="token punctuation">.</span><span class="token keyword">get</span><span class="token punctuation">(</span><span class="token string">'posts'</span><span class="token punctuation">,</span> <span class="token string">'PostsController.index'</span><span class="token punctuation">)</span>
-Route<span class="token punctuation">.</span><span class="token function">post</span><span class="token punctuation">(</span><span class="token string">'posts'</span><span class="token punctuation">,</span> <span class="token string">'PostsController.store'</span><span class="token punctuation">)</span></code></pre>`
+Route<span class="token punctuation">.</span><span class="token function">post</span><span class="token punctuation">(</span><span class="token string">'posts'</span><span class="token punctuation">,</span> <span class="token string">'PostsController.store'</span><span class="token punctuation">)</span></code></pre>`,
   },
   {
     title: 'Controllers',
@@ -175,7 +180,7 @@ Route<span class="token punctuation">.</span><span class="token function">post</
 
     <span class="token keyword">return</span> post
   <span class="token punctuation">}</span>
-<span class="token punctuation">}</span></code></pre>`
+<span class="token punctuation">}</span></code></pre>`,
   },
   {
     title: 'Models',
@@ -190,8 +195,8 @@ Route<span class="token punctuation">.</span><span class="token function">post</
 
   @<span class="token function">column</span><span class="token punctuation">(</span><span class="token punctuation">)</span>
   <span class="token keyword">public</span> body<span class="token operator">:</span> <span class="token builtin">string</span>
-<span class="token punctuation">}</span></code></pre>`
-  }
+<span class="token punctuation">}</span></code></pre>`,
+  },
 ]
 
 exports.edgeShowCaseTabs = [
@@ -201,7 +206,7 @@ exports.edgeShowCaseTabs = [
 <span class="token tag"><span class="token function">  @if</span>(user)</span>
     <span class="token other"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>p</span><span class="token punctuation">></span></span> Hello</span> <span class="token mustache"><span class="token punctuation">{{</span> user.username <span class="token punctuation">}}</span></span> <span class="token other"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>p</span><span class="token punctuation">></span></span></span>
 <span class="token tag"><span class="token function">  @endif</span></span>
-<span class="token other"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>body</span><span class="token punctuation">></span></span></span></code></pre>`
+<span class="token other"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>body</span><span class="token punctuation">></span></span></span></code></pre>`,
   },
   {
     title: 'Multiline Mustache',
@@ -211,7 +216,7 @@ exports.edgeShowCaseTabs = [
       <span class="token keyword">return</span> earning <span class="token operator">*</span> <span class="token number">100</span>
     <span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">.</span><span class="token function">join</span><span class="token punctuation">(</span><span class="token string">','</span><span class="token punctuation">)</span>
   }}
-<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>body</span><span class="token punctuation">></span></span></span></code></pre>`
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>body</span><span class="token punctuation">></span></span></span></code></pre>`,
   },
   {
     title: 'Components',
@@ -226,8 +231,8 @@ exports.edgeShowCaseTabs = [
       <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>a</span> <span class="token attr-name">href</span><span class="token attr-value"><span class="token punctuation">=</span><span class="token punctuation">"</span><span class="token punctuation">"</span></span><span class="token punctuation">></span></span> Yes, delete it <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>a</span><span class="token punctuation">></span></span></span>
 <span class="token tag"><span class="token function">    @endslot</span></span><span class="token tag"><span class="token function">
   @endcomponent</span></span>
-<span class="token other"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>body</span><span class="token punctuation">></span></span></span></code></pre>`
-  }
+<span class="token other"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>body</span><span class="token punctuation">></span></span></span></code></pre>`,
+  },
 ]
 
 /*
@@ -243,18 +248,18 @@ exports.primaryFeatures = [
   {
     title: 'Type-safe',
     icon: `<svg class="feature-icon" width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h40v40H0z"/><g transform="translate(1.25 1.25)" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5"><path d="M37.5 17.5C37.5 7.5 28.683 0 18.75 0 8.818 0 .71 7.73.062 17.5H15v6.25C15 29.962 20.038 35 26.25 35S37.5 29.962 37.5 23.75V17.5z" stroke="#17161A"/><path stroke="#5A45FF" d="M0 22.5v10L12.5 30"/><circle stroke="#5A45FF" cx="26.25" cy="23.75" r="3.75"/></g></g></svg>`,
-    body: `Type safety is baked into the framework with first class support for TypeScript. No need to install any additional build tools, TypeScript just works with AdonisJS.`
+    body: `Type safety is baked into the framework with first class support for TypeScript. No need to install any additional build tools, TypeScript just works with AdonisJS.`,
   },
   {
     title: 'Extensible',
     icon: `<svg class="feature-icon" width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h40v40H0z"/><g stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5"><path d="M21.667 8.333h6.666a3.333 3.333 0 013.334 3.334v6.666M8.333 21.667v6.666a3.333 3.333 0 003.334 3.334h6.666" stroke="#17161A"/><path stroke="#5A45FF" d="M1.667 1.667H15V15H1.667zM25 25h13.333v13.333H25z"/></g></g></svg>`,
-    body: `Instead of writing the same code across multiple projects, build your own first class primitives on top of the framework core.`
+    body: `Instead of writing the same code across multiple projects, build your own first class primitives on top of the framework core.`,
   },
   {
     title: 'Ton of Packages',
     icon: `<svg class="feature-icon" width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h40v40H0z"/><g stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5"><path stroke="#17161A" d="M3.333 3.334H15V15H3.333z"/><path stroke="#5A45FF" d="M30.833.918l8.25 8.25-8.25 8.25-8.25-8.25z"/><path stroke="#17161A" d="M25 25h11.667v11.667H25zM3.333 25H15v11.667H3.333z"/></g></g></svg>`,
-    body: `Stop spending hours to find the perfect NPM packages. AdonisJS comes with a ton of first party packages with unified API semantics.`
-  }
+    body: `Stop spending hours to find the perfect NPM packages. AdonisJS comes with a ton of first party packages with unified API semantics.`,
+  },
 ]
 
 /*
@@ -288,7 +293,7 @@ exports.secondaryFeatures = [
         title: 'JSON Serializers',
         icon: `<svg class="feature-icon" width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h40v40H0z"/><g stroke-linecap="round" stroke-linejoin="round"><path stroke="#17161A" stroke-width="2.5" d="M35 35.059V11.25l-10-10H5v33.809"/><path stroke="#5A45FF" stroke-width="2.5" d="M25 1.25v10h10z"/><path stroke="#17161A" stroke-width="2.5" d="M5 33.75v5h30v-5"/><path d="M10.095 30.244a3.946 3.946 0 01-.845-.087v-1.278c.089.015.182.035.281.055.106.022.214.033.322.032a.658.658 0 00.578-.246c.138-.258.198-.55.174-.84v-5.63h1.638v5.489a2.834 2.834 0 01-.537 1.874 1.993 1.993 0 01-1.61.631zM17.249 26.397c.006.342-.088.677-.27.964a1.78 1.78 0 01-.778.655 2.837 2.837 0 01-1.192.233 4.753 4.753 0 01-.958-.082 3.483 3.483 0 01-.801-.285v-1.405c.293.154.6.275.919.36.285.083.58.127.876.13.175.014.35-.028.5-.12a.377.377 0 00.096-.52.768.768 0 00-.207-.18 9.166 9.166 0 00-.76-.376 3.602 3.602 0 01-.84-.503 1.536 1.536 0 01-.414-.559 1.883 1.883 0 01-.135-.746c-.021-.49.193-.96.575-1.258a2.498 2.498 0 011.58-.45c.626.007 1.242.15 1.81.418l-.473 1.218a3.367 3.367 0 00-1.384-.376.712.712 0 00-.438.109.33.33 0 00-.137.267.39.39 0 00.174.307c.301.19.617.357.945.496.395.154.747.404 1.026.728.198.284.298.627.286.975z" fill="#5A45FF" fill-rule="nonzero"/><path d="M23.242 25.248a3.027 3.027 0 01-.762 2.231 3.02 3.02 0 01-2.23.764 3.014 3.014 0 01-2.22-.768 3.012 3.012 0 01-.771-2.24 2.998 2.998 0 01.768-2.218 3.032 3.032 0 012.23-.76 3.017 3.017 0 012.226.76c.548.623.82 1.423.759 2.231zm-4.233 0c0 1.124.413 1.686 1.24 1.686a1.09 1.09 0 00.937-.407c.234-.387.34-.832.305-1.277a2.162 2.162 0 00-.309-1.287 1.081 1.081 0 00-.924-.416c-.832-.001-1.248.566-1.25 1.701zM30.246 28.25h-2.23l-2.3-4.227h-.033c.055.664.082 1.172.082 1.523v2.704H24.25v-6h2.222l2.298 4.17h.025c-.04-.605-.06-1.091-.06-1.457V22.25h1.515l-.004 6z" fill="#5A45FF" fill-rule="nonzero"/></g></g></svg>`,
         body: `AdonisJS is the only Node.js framework that has first class support for JSON:API. If your team has settled on using JSON:API, then adding it to backend is just a matter of few minutes.`,
-      }
+      },
     ],
   },
   {
@@ -313,7 +318,7 @@ exports.secondaryFeatures = [
         title: 'Seeds & Factories',
         icon: `<svg class="feature-icon" width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><circle id="a" cx="8" cy="8" r="8"/></defs><g fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><g transform="translate(2.333 22.333)" stroke-width="2.5"><rect stroke="#17161A" x=".667" y=".667" width="29" height="14" rx="3.333"/><path stroke="#5A45FF" d="M6.667 7.667h7M22.667 7.667h1"/></g><g transform="translate(2.33 8)" stroke-width="2.5"><rect stroke="#17161A" x=".667" y=".667" width="29" height="14" rx="3.333"/><path stroke="#5A45FF" d="M6.667 7.667h7M26.667 7.667h1"/></g><g transform="translate(22 2)"><use stroke="#17161A" stroke-width="4" fill="#FFF" xlink:href="#a"/><circle stroke="#FFF" stroke-width="2.1" cx="8" cy="8" r="9.05"/></g><path stroke="#5A45FF" stroke-width="2.5" d="M30 7v6M27 10h6"/></g></svg>`,
         body: `No need to share SQL dump with your team mates anymore. Instead, use the database seeders to quickly seed your database with dummy data.`,
-      }
+      },
     ],
   },
   {
@@ -348,7 +353,7 @@ exports.secondaryFeatures = [
         title: 'CORS Enabled',
         icon: `<svg class="feature-icon" width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h40v40H0z"/><g stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5"><path stroke="#5A45FF" d="M13.75 20h12.5v8.75h-12.5zM16.25 20v-3.75A3.75 3.75 0 0120 12.5h0a3.75 3.75 0 013.75 3.75V20"/><path d="M35 23.75c0 8.284-6.716 15-15 15-8.284 0-15-6.716-15-15V6.25L20 2.5l15 3.75v17.5z" stroke="#17161A"/></g></g></svg>`,
         body: `AdonisJS let you manage the security settings for <strong>Cross Origin HTTP Requests</strong> to make sure that only authorized and valid requests are making their way through the application.`,
-      }
+      },
     ],
   },
   {
@@ -363,7 +368,7 @@ exports.secondaryFeatures = [
         title: 'Unified Profiler',
         icon: `<svg class="feature-icon" width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h40v40H0z"/><g stroke-linecap="round" stroke-linejoin="round" transform="translate(3.125 3.125)" stroke="#000" stroke-width="2.5"><path d="M33.75 33.75L23.473 23.473"/><circle cx="13.75" cy="13.75" r="13.75"/></g><g><path d="M9 9h16v16H9z"/><g stroke-linecap="round" stroke-linejoin="round" stroke="#5A45FF" stroke-width="2.5"><path d="M11.667 20.333l4-4-4-4M17 21.666h5.334"/></g></g></g></svg>`,
         body: `The framework comes with an embeddable profiler (with minimum overhead) to time different areas of your application and find potential bottlenecks`,
-      }
+      },
     ],
-  }
+  },
 ]
